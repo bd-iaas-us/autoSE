ecf83bb310996d06f80d00fd1b8aedc94a605a27
dongmao.zhang <dongmao.zhang@bytedance.com>
2024-06-06 17:00:53 -0700

    add cargo parse_se_log

--- server/april/ui.py
+++ server/april/ui.py
@@ -4,35 +4,48 @@ import metrics
 import argparse
 
 
+has_parse_se_logs = False
+try:
+    import parse_se_logs
+    has_parse_se_logs = True
+except:
+    import metrics
+
 def main(traj_dir :str, lint_file: str):
     st.title("Log Visualization")
 
-    traj_points = metrics.parse_swe_traj(traj_dir)
-    lint_points = metrics.parse_lint(lint_file)
+    if has_parse_se_logs:
+        lint_points = parse_se_logs.parse_lint_log(lint_file)
+        traj_points = parse_se_logs.parse_traj_log(traj_dir)
+        df_traj = pd.DataFrame(traj_points)
+        df_lint = pd.DataFrame(lint_points)
+    else:
+        traj_points = metrics.parse_swe_traj(traj_dir)
+        df_traj = pd.DataFrame([{
+            'time': tp.time,
+            'api_calls': tp.api_calls,
+            'tokens_sent': tp.tokens_sent,
+            'tokens_received': tp.tokens_received,
+            'total_cost': tp.total_cost,
+            'instance_cost': tp.instance_cost,
+            'exit_status': tp.exit_status
+        } for tp in traj_points])
+        lint_points = metrics.parse_lint(lint_file)
+        df_lint = pd.DataFrame([{"time":lp.time, "dur": lp.dur} for lp in lint_points])
 
 
-    df = pd.DataFrame([{
-        'time': tp.time,
-        'api_calls': tp.api_calls,
-        'tokens_sent': tp.tokens_sent,
-        'tokens_received': tp.tokens_received,
-        'total_cost': tp.total_cost,
-        'instance_cost': tp.instance_cost,
-        'exit_status': tp.exit_status
-    } for tp in traj_points])
 
     st.write(f'### total dev requests {len(traj_points)}')
     st.write("### Open API Calls Over Time")
-    st.bar_chart(df[['time', 'api_calls']], x="time")
+    st.bar_chart(df_traj[['time', 'api_calls']], x="time")
 
     st.write("### Open API tokens send/received Over Time")
-    st.bar_chart(df[['time', 'tokens_sent', 'tokens_received']], x="time")
+    st.bar_chart(df_traj[['time', 'tokens_sent', 'tokens_received']], x="time")
 
 
     #only time.
     st.write("### when lint is invoked")
-    df = pd.DataFrame([{"time":lp.time, "dur": lp.dur} for lp in lint_points])
-    st.scatter_chart(df, x='time')
+    st.scatter_chart(df_lint, x='time')
 
 if __name__ == "__main__":
     """
